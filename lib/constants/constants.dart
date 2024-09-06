4b08f1390eb2d735564f7582d4e5228df13f4d41
MiladSm79 <miladms666@gmail.com>
2024-09-06 17:17:41 +0330

    added constants file that stores some text and colors

---
+++ lib/constants/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class constants {
+  static Color primaryColor = Colors.green;
+  static Color blackColor = Colors.black;
+  
+  static String titleOne = "گیاهان را بهتر از قبل درک کن";
+  static String descriptionOne = "در مورد نگه داری گل و گیاه میتونی اطلاعات کسب کنی ";
+  static String titleTwo = " میتوانید با گیاهان جدید آشنا شوید ";
+  static String decriptionTwo = "رز مشکی یا گل رز دوست داری ؟ اینجا میتونی پیداش کنی";
+  static String titleThree = "با یک گل بهار نمیشود . گل بکار";
+  static String decriptionThree = "هر گلی نیاز داشته باشید در این اپلیکیشن پیدا میکنید";
+  
+}
